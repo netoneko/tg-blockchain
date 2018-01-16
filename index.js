cc75ce0d6675d6f80e02a1ac89c6cba2f50c34cc
Kirill Maksimov <kirill.maksimov@ironsrc.com>
2018-01-16 21:41:30 +0200

    Write blocks

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const _ = require('lodash'),
     DEFAULT_EMAIL = process.env.DEFAULT_EMAIL || 'noreply@example.com';
 
 const updateMessage = (message) => {
-    const tokens = message.split(' '),
+    const tokens = message.split('\n'),
         seed = _.parseInt(tokens[0]) || 0;
 
     tokens[0] = seed + 1;
-    return tokens.join(' ');
+    return tokens.join('\n');
 };
 
 const proofOfWork = (repo, oid, update_ref) => {
@@ -35,7 +35,6 @@ const proofOfWork = (repo, oid, update_ref) => {
 };
 
 const commit = (repo, message, parent) => {
-    console.log(repo, message)
     const append = (index) => {
         return index.addByPath('blank').then(() => {
             return index.writeTree();
@@ -59,7 +58,7 @@ const getRepo = (path) => {
     if (!fs.exists(path)) {
         return git.Repository.init(path, 0).then(open).then(repo => {
             fs.writeFileSync(`${path}/blank`, '');
-            return commit(repo, '0 Initial commit');
+            return commit(repo, '0\n\nInitial commit');
         }).then(open);
     }
 
@@ -73,21 +72,38 @@ const getHEAD = (repo) => {
 };
 
 const formatMessage = (msg) => {
-    const name = _.trim(`${msg.from.first_name || ''} ${msg.from.last_name || ''}`);
-    return `0 ${name} <@${msg.from.username}>: ${msg.text}`;
+    const name = _.trim(`${msg.from.first_name || ''} ${msg.from.last_name || ''}`),
+        date = new Date(msg.date * 1000).toISOString();
+    return `${name} <@${msg.from.username}> ${date}: ${msg.text}`;
+};
+
+const createBlock = (repo, queue) => {
+    if (_.isEmpty(queue)) return;
+
+    console.log(`Creating new block, size: ${queue.length}`);
+
+    const values = [];
+    while (queue.length > 0) {
+        values.push(queue.pop());
+    }
+
+    const formatted = _(values).sortBy('date').map(formatMessage).value();
+    formatted.unshift('0\n');
+
+    getHEAD(repo).then(head => {
+        return commit(repo, formatted.join('\n'), head);
+    }).catch(console.log);
 };
 
 const createBot = (token, repo) => {
-    const bot = new TelegramBot(token, {polling: true});
+    const bot = new TelegramBot(token, {polling: true}),
+        queue = [];
+
+    setInterval(_.partial(createBlock, repo, queue), 10000);
 
     bot.on('message', (msg) => {
-      const chatId = msg.chat.id;
-
       console.log(msg);
-
-      getHEAD(repo).then(head => {
-          return commit(repo, formatMessage(msg), head);
-      }).catch(console.log);
+      queue.push(msg);
     });
 };
 
